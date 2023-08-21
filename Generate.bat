6854ce6dac37d3dc524eafdbe43eeb90a3c34739
Jason Zheng <quan3969@qq.com>
2023-08-21 17:00:00 +0800

    Generate update
     - support USB disk unplug during flash

--- Generate.bat
+++ Generate.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 rem Fine BIOS update files and generate script for easy update.
 rem Presented by Q3aN.
-rem 2023.07.06
+rem 2023.07.28
 
 rem BIN
 for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".bin"`) do (
@@ -12,8 +12,56 @@ for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".bin"`) do (
     if "!file_name:~13!" EQU "" if "!file_name:~12,1!" NEQ "" if "!file_name:~0,1!" EQU "E" (
         set outname=!file_name:~6,3!_BIN.nsh
         rem MTL
-        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA"`) do (
-            echo Fpt_MTL.efi /f !file_name! > !outname!
+        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA JZP"`) do (
+            echo echo -off> !outname!
+            echo cls>> !outname!
+            echo.>> !outname!
+            echo set -v BiosFile !file_name!>> !outname!
+            echo.>> !outname!
+            echo for %%n% in 0 1 2 3 4 5 6 7 >> !outname!
+            echo     if exist fs%%n%:\EFI\Microsoft\Boot\bootmgr.efi then>> !outname!
+            echo         set -v DiskNo %%n%>> !outname!
+            echo         goto FoundDisk>> !outname!
+            echo     endif>> !outname!
+            echo endfor>> !outname!
+            echo goto NotFoundDisk>> !outname!
+            echo.>> !outname!
+            echo :FoundDisk>> !outname!
+            echo if exist fs%%DiskNo%%:\BIOS then>> !outname!
+            echo     rm -q fs%%DiskNo%%:\BIOS ^> nul>> !outname!
+            echo endif>> !outname!
+            echo mkdir fs%%DiskNo%%:\BIOS ^> nul>> !outname!
+            echo cp -q %%cwd%%\Fpt_MTL.efi fs%%DiskNo%%:\BIOS\ ^> nul>> !outname!
+            echo cp -q %%cwd%%\%%BiosFile%% fs%%DiskNo%%:\BIOS\ ^> nul>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo = Now flashing: %%BiosFile%%>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo " ">> !outname!
+            echo fs%%DiskNo%%:\BIOS\Fpt_MTL.efi -f fs%%DiskNo%%:\BIOS\%%BiosFile%%>> !outname!
+            echo rm -q fs%%DiskNo%%:\BIOS\%%BiosFile%% ^> nul>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo = Reboot now, please wait...>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo " ">> !outname!
+            echo fs%%DiskNo%%:\BIOS\Fpt_MTL.efi -greset ^> nul>> !outname!
+            echo goto End>> !outname!
+            echo.>> !outname!
+            echo :NotFoundDisk>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo = Now flashing: %%BiosFile%%>> !outname!
+            echo echo = Please don't unlpug usb disk...>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo " ">> !outname!
+            echo Fpt_MTL.efi -f %%BiosFile%%>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo = Reboot now, please wait...>> !outname!
+            echo echo =====================================================>> !outname!
+            echo echo " ">> !outname!
+            echo Fpt_MTL.efi -greset ^> nul>> !outname!
+            echo goto End>> !outname!
+            echo.>> !outname!
+            echo :End>> !outname!
+            echo echo -on>> !outname!
         )
         rem RPL
         for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RGU RGS RGT"`) do (
@@ -36,8 +84,9 @@ for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".bin"`) do (
     if "!file_name:~13!" EQU "" if "!file_name:~12,1!" NEQ "" if "!file_name:~0,1!" NEQ "E" (
         set outname=!file_name:~0,3!_BIN.nsh
         rem RPL
-        for /f "usebackq" %%a in (`echo %%i ^| findstr /i "RHA"`) do (
+        for /f "usebackq" %%a in (`echo %%i ^| findstr /i "RHA JZP"`) do (
             echo Fpt_MTL.efi /f !file_name! > !outname!
+            echo Fpt_MTL.efi /greset >> !outname!
         )
         rem RPL
         for /f "usebackq" %%a in (`echo %%i ^| findstr /i "RGU RGS RGT"`) do (
@@ -65,7 +114,7 @@ for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".cap"`) do (
     if "!file_name:~13!" EQU "" if "!file_name:~12,1!" NEQ "" if "!file_name:~0,1!" EQU "E" (
         set outname=!file_name:~6,3!_CAP.nsh
         rem MTL
-        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA"`) do (
+        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA JZP"`) do (
             echo AfuEfix64_MTL.efi !file_name! /p /b /n /r /e /capsule /q > !outname!
         )
         rem RPL
@@ -114,7 +163,7 @@ for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".exe"`) do (
     if "!file_name:~18!" EQU "" if "!file_name:~17,1!" NEQ "" if "!file_name:~4,1!" EQU "E" (
         set outname=!file_name:~11,3!_EXE.bat
         rem MTL
-        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA"`) do (
+        for /f "usebackq delims=" %%a in (`echo %%i ^| findstr /i "RHA JZP"`) do (
             echo !file_name! /eu:Venus4 > !outname!
         )
         rem RPL
@@ -138,7 +187,7 @@ for /f "usebackq delims=" %%i in (`dir /b ^| findstr /i /c:".exe"`) do (
     if "!file_name:~18!" EQU "" if "!file_name:~17,1!" NEQ "" if "!file_name:~4,1!" NEQ "E" (
         set outname=!file_name:~4,3!_EXE.bat
         rem MTL
-        for /f "usebackq" %%a in (`echo %%i ^| findstr /i "RHA"`) do (
+        for /f "usebackq" %%a in (`echo %%i ^| findstr /i "RHA JZP"`) do (
             echo !file_name! /eu:Venus4 > !outname!
         )
         rem RPL
