936a59c2eff4c2668e5bdea0dd361e05aa0cefad
Jason Zheng <quan3969@qq.com>
2024-04-02 22:58:07 +0900

    BuildGather update
    - ui update
    - use errorlevel and error handler
    - support test build gather
    - support cat and cab file gather

--- BuildGather.bat
+++ BuildGather.bat
@@ -1,36 +1,35 @@
 @echo off
 setLocal enableDelayedExpansion
-rem By Q3aN 240131
-set ver=v02
+rem By Q3aN 240401
+set ver=v03
 
 echo.
 echo =====================================================
-echo Welcome to BuildGather %ver%, gather build output:
-echo.
+echo ^>
+echo ^> Welcome to BuildGather %ver%
 
 call :Save_Buildfiles
+call :Do_Ending %errorlevel%
 
-echo.
+echo ^>
 echo =====================================================
 echo.
 
+if "%end_pause%" EQU "1" ( pause )
 endLocal
-exit /b 0
+exit /b
+
 
 rem ****************************************************************************
 rem Check and save build output files
 :Save_Buildfiles
-setLocal enableDelayedExpansion
 :: 1. Check build complete
 if not exist Build\Token.h (
-    echo.
-    echo ^> Project may bot be compiled.
-    echo.
-    pause
-    exit /b 0
+    exit /b 6
 )
-for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"DEBUG_MODE" Build\Token.h`) do (
-    set Debug_Mode=%%i
+set Debug_Mode=0
+for /f "usebackq delims=" %%i in (`findstr /c:"#define DEBUG_MODE	1" Build\Token.h`) do (
+    set Debug_Mode=1
 )
 :: 2. Check Samsung Project
 for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"SAMSUNG_BIOS_MAJOR_VERSION" Build\Token.h`) do (
@@ -39,9 +38,13 @@ for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"SAMSUNG_BIOS_MAJOR_
 for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"SAMSUNG_BIOS_MINOR_VERSION" Build\Token.h`) do (
     set Ver_Minor=%%i
 )
+set test_build=4
+for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"SAMSUNG_TEST_BIOS_VERSION" Build\Token.h`) do (
+    set test_build=%%i
+)
 if "%Ver_Major%" NEQ "" (
-    call :Save_Samsung %Ver_Major% %Ver_Minor% %Debug_Mode%
-    exit /b 0
+    call :Save_Samsung %Ver_Major% %Ver_Minor% %Debug_Mode% %test_build%
+    exit /b
 )
 :: 3. Check AMI project
 for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"RECOVERY_ROM" Build\Token.h`) do (
@@ -49,26 +52,43 @@ for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"RECOVERY_ROM" Build
 )
 if "%Rom_Name%" NEQ "" (
     call :Save_AMI %Rom_Name%
-    exit /b 0
+    exit /b
 )
-endLocal
-exit /b 0
+exit /b
+
+
+rem ****************************************************************************
+rem Do the ending and clean up job
+rem %~1: Ending reason:
+rem        0 - Success
+rem        6 - Project not complied yet
+:Do_Ending
+set end_pause=1
+if "%~1" EQU "0" ( echo ^>
+    echo ^> Success
+    set end_pause=0
+) else if "%~1" EQU "6" ( echo ^>
+    echo ^> Project may not be compiled.
+)
+exit /b
+
 
 rem ****************************************************************************
 rem Save build output files for Samsung Project
 rem %~1: Major version
 rem %~2: Minor version
 rem %~3: Debug mode
+rem %~4: Test build (%~4 = 4 means not found)
 :Save_Samsung
-setLocal enableDelayedExpansion
-echo.
-echo ^> Gather files for Samsung project.
-echo.
+echo ^>
+echo ^> Gather for Samsung project.
 :: for E00JZR_01A
+set Rom_Name=%~1_%~2
+if "%~4" NEQ "4" (
+    set Rom_Name=%Rom_Name%_%~4
+)
 if "%~3" EQU "1" (
-    set Rom_Name=%~1_%~2_dbg
-) else (
-    set Rom_Name=%~1_%~2
+    set Rom_Name=%Rom_Name%_dbg
 )
 :: for E00JZR01A
 for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"FWCAPSULE_FILE_NAME" Build\Token.h`) do (
@@ -77,16 +97,18 @@ for /f "usebackq tokens=2 delims=	." %%i in (`findstr /i /c:"FWCAPSULE_FILE_NAME
 if %~1%~2 EQU %Version_Now_Using% (
     set Rom_Name=%~1%~2
 )
-if exist %Rom_Name% (del /q %Rom_Name%\*) else (mkdir %Rom_Name%)
+if exist %Rom_Name% (del /s/q %Rom_Name%\*) else (mkdir %Rom_Name%)
 if exist Build.log xcopy Build.log %Rom_Name%\
 if exist Build\Token.h xcopy Build\Token.h %Rom_Name%\
 if exist Build\Token.mak xcopy Build\Token.mak %Rom_Name%\
-if exist %Rom_Name%.BIN xcopy %Rom_Name%.BIN %Rom_Name%\
+if exist Build\Platform.fdf xcopy Build\Platform.fdf %Rom_Name%\
+if exist %Rom_Name%.bin xcopy %Rom_Name%.bin %Rom_Name%\
 if exist %Rom_Name%.map xcopy %Rom_Name%.map %Rom_Name%\
 if exist %~1.map xcopy %~1.map %Rom_Name%\
 if exist %Rom_Name%.CAP xcopy %Rom_Name%.CAP %Rom_Name%\
-if exist WIN_%~1_%~2.exe xcopy WIN_%~1_%~2.exe %Rom_Name%\
-if exist WIN_%~1_%~2_dbg.exe xcopy WIN_%~1_%~2.exe %Rom_Name%\
+if exist WIN_%Rom_Name%.exe xcopy WIN_%Rom_Name%.exe %Rom_Name%\
+if exist Build\AmiCrbMeRoms\ME_FWUpdate.bin xcopy Build\AmiCrbMeRoms\ME_FWUpdate.bin %Rom_Name%\
+if exist Build\AmiCrbMeRoms\ME_FWUpdate.map xcopy Build\AmiCrbMeRoms\ME_FWUpdate.map %Rom_Name%\
 for /f "usebackq delims=" %%i in (`dir /b/s SetupDefaults.i ^| findstr /i /c:"Build"`) do (
     if "%%i" NEQ "" (xcopy %%i %Rom_Name%\)
 )
@@ -96,9 +118,13 @@ for /f "usebackq tokens=1 delims=." %%i in (`dir /b ^| ^(findstr /i /c:".veb"^)`
 for /f "usebackq delims=" %%i in (`dir /b/s "%POJ_Name%.map" ^| findstr /i /c:"Build"`) do (
     if exist %%i xcopy %%i %Rom_Name%\
 )
+for /f "usebackq delims=" %%i in (`dir /b *.cab ^| findstr /i /c:"%~1"`) do (
+    if exist %%i xcopy %%i %Rom_Name%\
+)
+if exist WFU/%~1.cat xcopy WFU %Rom_Name%\WFU\
 explorer %~dp0%ROM_Name%
-endLocal
 exit /b 0
+
 
 rem ****************************************************************************
 rem Save build output files for AMI Project
@@ -106,12 +132,12 @@ rem %~1: Rom name
 :Save_AMI
 setLocal enableDelayedExpansion
 echo.
-echo ^> Gather files for CRB project.
-echo.
+echo ^> Gather for AMI project.
 if exist %~1 (del /q /s %~1\*) else (mkdir %~1)
 if exist Build.log xcopy Build.log %~1\
 if exist Build\Token.h xcopy Build\Token.h %~1\
 if exist Build\Token.mak xcopy Build\Token.mak %~1\
+if exist Build\Platform.fdf xcopy Build\Platform.fdf %Rom_Name%\
 if exist Build\AmiCrbMeRoms xcopy Build\AmiCrbMeRoms %~1\AmiCrbMeRoms\
 if exist %~1.rom xcopy %~1.rom %~1\
 for /f "usebackq delims=" %%i in (`dir /b/s SetupDefaults.i ^| findstr /i /c:"Build"`) do (
@@ -125,4 +151,4 @@ for /f "usebackq delims=" %%i in (`dir /b/s "%POJ_Name%.map" ^| findstr /i /c:"B
 )
 explorer %~dp0%~1
 endLocal
-exit /b 0
+exit /b
