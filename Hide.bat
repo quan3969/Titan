55f6b88c657118c5ad269c848891d25e7f327f09
Jason Zheng <quan3969@qq.com>
2024-07-04 22:44:39 +0900

    Hide update

--- Hide.bat
+++ Hide.bat
@@ -1,3 +1,7 @@
 @echo off
-for /f "usebackq delims=" %%d in (`dir /b ^| findstr /v "Generate.bat 0-Temp 1-Apps 2-Drivers 3-BIOS 4-Misc"`) do attrib "%%d" +s +h
-del Hide.bat
+if exist UnHide.bat del UnHide.bat
+for /f "usebackq delims=" %%d in (`dir /b/a ^| findstr /v "Generate.bat Archive"`) do attrib "%%d" +s +h
+
+echo @echo off > Unhide.bat
+echo for /f "usebackq delims=" %%%%d in (`dir /b/a ^^^| findstr /v "Generate.bat Archive"`) do attrib "%%%%d" -s -h >> Unhide.bat
+echo if exist UnHide.bat del UnHide.bat >> Unhide.bat
